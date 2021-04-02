ee4f52ee41d186a63cada0f726aaedb1bdbe77da
Xue Li <xueli@Xues-MBP.lan>
2021-04-02 13:04:38 +0100

    added item to menu and fixed mobile menu

--- Resources/Menu.js
+++ Resources/Menu.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
     $(".burger-nav").on("click", function(){
-        $("header nav ul").toggleClass("open");
+        $("header nav ul").toggleClass("closed");
     });
 });
+
+const desktopMenu = document.getElementById("desktop-menu");
+const menuItems = document.getElementsByClassName("menu-item");
+const burgerNav = document.getElementById("burger-nav");
+
+if (!desktopMenu.className.includes("closed")) {
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener("click", () => desktopMenu.className = "desktop");
+        menuItems[i].removeEventListener("click", () => desktopMenu.className = "desktop");
+    }
+}
